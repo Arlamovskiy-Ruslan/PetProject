db32ad996a0d829c7d34b56376fba323663f3131
Arktee <55766007+Arktee@users.noreply.github.com>
2021-01-09 18:51:05 +0200

    Initial Commit

--- project/src/main/java/com/pet/project/controllers/RecordController.java
+++ project/src/main/java/com/pet/project/controllers/RecordController.java
@@ -1,29 +1,45 @@
 package com.pet.project.controllers;
 
+import com.pet.project.models.User;
 import com.pet.project.models.UserRecord;
 import com.pet.project.repo.UserRecordRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.pet.project.repo.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+
 @Controller
 public class RecordController {
-    @Autowired
-    private UserRecordRepo userRecordRepo;
+
+    private static final Logger logger = LoggerFactory.getLogger(RecordController.class);
+
+    private final UserRecordRepo userRecordRepo;
+
+    private final UserRepo userRepo;
+
+    public RecordController(UserRecordRepo userRecordRepo, UserRepo userRepo) {
+        this.userRecordRepo = userRecordRepo;
+        this.userRepo = userRepo;
+    }
 
     @GetMapping("/records")
-    public String records(Model model) {
-        Iterable<UserRecord> users_rec = userRecordRepo.findAll();
-        model.addAttribute("users_rec", users_rec);
+    public String records(Model model , Principal principal) {
+        logger.info("User name: {}", principal.getName());
+        model.addAttribute("users_rec", userRecordRepo.findByUserUsername(principal.getName()));
         return "records";
     }
 
     @GetMapping("/record-add")
     public String reсord_add(Model model) {
+
         return "record-add";
+
     }
 
 
@@ -33,7 +49,10 @@ public class RecordController {
             ,@RequestParam String name
             ,@RequestParam String last_name
             ,@RequestParam String problem
-            , Model model) {
+            , UserRecord record , Principal principal) {
+        User user = userRepo.findByUsername(principal.getName()).get();
+        record.setUser(user);
+        userRecordRepo.save(record);
         UserRecord user_rec_add = new UserRecord(first_name,name,last_name,problem);
         userRecordRepo.save(user_rec_add);
         return "redirect:/records";
--- project/src/main/java/com/pet/project/repo/UserRecordRepo.java
+++ project/src/main/java/com/pet/project/repo/UserRecordRepo.java
@@ -4,6 +4,10 @@ import com.pet.project.models.UserRecord;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRecordRepo extends CrudRepository<UserRecord,Long> {
+    
+    List<UserRecord> findByUserUsername(String username);
 }
