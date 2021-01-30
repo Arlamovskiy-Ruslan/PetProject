ccc14f8f482980965f2d3bff41c9bb7d9468b933
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-01-30 19:20:08 +0200

    Tests

--- project/src/test/java/com/pet/project/ProjectApplicationTests.java
+++ project/src/test/java/com/pet/project/ProjectApplicationTests.java
@@ -1,13 +1,53 @@
 package com.pet.project;
 
+import com.pet.project.controllers.MainController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ProjectApplicationTests {
+@AutoConfigureMockMvc
+public class ProjectApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private MainController mainController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect((ResultMatcher) content().string(containsString("Hello guest")));
 	}
 
+	@Test
+	public void loginTest() throws Exception {
+		this.mockMvc.perform(get("/home"))
+				.andDo(print())
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("http:localhost/login"));
+	}
+
+	@Test
+	public void correctLoginTest() throws Exception {
+		this.mockMvc.perform(formLogin().user("k").password("k"))
+		.andDo(print())
+		.andExpect(status().is3xxRedirection())
+		.andExpect(redirectedUrl("/"));
+	}
 }
