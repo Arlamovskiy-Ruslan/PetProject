8420511f9dcc784eed29b5d7c4ba3d71ccce8d83
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-04-30 16:45:27 +0300

    Initial Commit

--- project/src/main/java/com/pet/project/controllers/MainController.java
+++ project/src/main/java/com/pet/project/controllers/MainController.java
@@ -1,20 +1,34 @@
 package com.pet.project.controllers;
 
+import com.pet.project.models.ExcelView;
+import com.pet.project.models.UserRecord;
+import com.pet.project.repo.UserRecordRepo;
 import com.pet.project.service.RecordService;
 import com.pet.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+    private final UserRecordRepo userRecordRepo;
+
+    @Autowired
+    public MainController(UserRecordRepo userRecordRepo) {
+        this.userRecordRepo = userRecordRepo;
+
+    }
 
     @Autowired
     UserService userService;
@@ -30,5 +44,11 @@ public class MainController {
         return "home";
     }
 
+    @RequestMapping(value = "/downloadExcel", method = RequestMethod.GET)
+    public ModelAndView downloadExcel(Model model) {
 
+        List<UserRecord> users = userRecordRepo.findAll();
+        return new ModelAndView(new ExcelView(), "user_rec", users );
+    }
 }
+
---
+++ project/src/main/java/com/pet/project/models/ExcelView.java
@@ -0,0 +1,57 @@
+package com.pet.project.models;
+
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+public class ExcelView extends AbstractXlsView {
+
+    @Override
+    public void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+                                   HttpServletResponse response) throws Exception {
+
+        response.setHeader("Content-Disposition", "attachment; filename=\"my-exported-file.xls\"");
+
+        @SuppressWarnings("unchecked")
+        List<UserRecord> user_rec= (List<UserRecord>) model.get("user_rec");
+
+        Sheet sheet = workbook.createSheet("Users Detail");
+        sheet.setDefaultColumnWidth(30);
+
+        CellStyle style = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setFontName("Arial");
+        style.setFillForegroundColor(HSSFColor.BLUE.index);
+        font.setColor(HSSFColor.BLACK.index);
+        style.setFont(font);
+
+        Row header = sheet.createRow(0);
+        header.createCell(0).setCellValue("First Name");
+        header.getCell(0).setCellStyle(style);
+        header.createCell(1).setCellValue("Name");
+        header.getCell(1).setCellStyle(style);
+        header.createCell(2).setCellValue("Last Name");
+        header.getCell(2).setCellStyle(style);
+        header.createCell(3).setCellValue("Problem");
+        header.getCell(3).setCellStyle(style);
+        header.createCell(4).setCellValue("Mail");
+        header.getCell(4).setCellStyle(style);
+
+        int rowCount = 1;
+        for(UserRecord userRecord : user_rec){
+            Row userRow =  sheet.createRow(rowCount++);
+            userRow.createCell(0).setCellValue(userRecord.getFirst_name());
+            userRow.createCell(1).setCellValue(userRecord.getName());
+            userRow.createCell(2).setCellValue(userRecord.getLast_name());
+            userRow.createCell(3).setCellValue(userRecord.getProblem());
+            userRow.createCell(4).setCellValue(userRecord.getMail());
+
+        }
+
+    }
+}
